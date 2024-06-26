16b2e44ead793a33c0aeb7c779b9fe68d64054a2
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-06-26 09:02:36 +0200

    Initial commit

---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# MycilaUtilities
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Continuous Integration](https://github.com/mathieucarbou/MycilaUtilities/actions/workflows/ci.yml/badge.svg)](https://github.com/mathieucarbou/MycilaUtilities/actions/workflows/ci.yml)
+[![PlatformIO Registry](https://badges.registry.platformio.org/packages/mathieucarbou/library/MycilaUtilities.svg)](https://registry.platformio.org/libraries/mathieucarbou/MycilaUtilities)
+
+Utils stuff for Arduino / ESP32 like Time, String functions, CircularBuffer, etc
---
+++ examples/Example1/Example1.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+#include <MycilaCircularBuffer.h>
+#include <MycilaString.h>
+#include <MycilaTime.h>
+
+Mycila::CircularBuffer<int, 5> buffer;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    continue;
+
+  buffer.add(1);
+  buffer.add(2);
+  buffer.add(3);
+  buffer.add(4);
+  buffer.add(5);
+  buffer.add(6);
+  buffer.add(7);
+
+  buffer.dump(Serial);
+  Serial.println();
+
+  Serial.println(Mycila::Str::lowerCaseCopy("Hello World"));
+  Serial.println(Mycila::Time::toDHHMMSS(180245));
+  Serial.println(Mycila::Time::toMinutes("12:34"));
+  Serial.println(Mycila::Time::getUnixTime());
+}
+
+// Destroy default Arduino async task
+void loop() { vTaskDelete(NULL); }
