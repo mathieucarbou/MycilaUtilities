0845ff5d5e5bcb2aeedf5d4d4acb5322f602348b
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-08-10 12:56:55 +0200

    Add versioning

---
+++ src/MycilaUtilities.h
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright (C) 2023-2024 Mathieu Carbou
+ */
+#pragma once
+
+#define MYCILA_UTILITIES_VERSION "1.3.6"
+#define MYCILA_UTILITIES_VERSION_MAJOR 1
+#define MYCILA_UTILITIES_VERSION_MINOR 3
+#define MYCILA_UTILITIES_VERSION_REVISION 6
+
+#include "MycilaCircularBuffer.h"
+#include "MycilaExpiringValue.h"
+#include "MycilaPID.h"
+#include "MycilaString.h"
+#include "MycilaTime.h"
