6c22c9bbe31345da8d7f9285ebb04785b8b78e26
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-10-15 23:42:20 +0200

    v4.0.0

--- src/MycilaUtilities.h
+++ src/MycilaUtilities.h
@@ -4,10 +4,10 @@
  */
 #pragma once
 
-#define MYCILA_UTILITIES_VERSION          "3.3.1"
-#define MYCILA_UTILITIES_VERSION_MAJOR    3
-#define MYCILA_UTILITIES_VERSION_MINOR    3
-#define MYCILA_UTILITIES_VERSION_REVISION 1
+#define MYCILA_UTILITIES_VERSION          "4.0.0"
+#define MYCILA_UTILITIES_VERSION_MAJOR    4
+#define MYCILA_UTILITIES_VERSION_MINOR    0
+#define MYCILA_UTILITIES_VERSION_REVISION 0
 
 #include "MycilaCircularBuffer.h"
 #include "MycilaExpiringValue.h"
