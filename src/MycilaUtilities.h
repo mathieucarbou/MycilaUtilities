69db2221342a5b8cc29d100b7cbb4fac327f4824
Mathieu Carbou <mathieu.carbou@gmail.com>
2026-05-17 17:07:10 +0200

    v5.0.0

--- src/MycilaUtilities.h
+++ src/MycilaUtilities.h
@@ -4,9 +4,9 @@
  */
 #pragma once
 
-#define MYCILA_UTILITIES_VERSION          "4.3.0"
-#define MYCILA_UTILITIES_VERSION_MAJOR    4
-#define MYCILA_UTILITIES_VERSION_MINOR    3
+#define MYCILA_UTILITIES_VERSION          "5.0.0"
+#define MYCILA_UTILITIES_VERSION_MAJOR    5
+#define MYCILA_UTILITIES_VERSION_MINOR    0
 #define MYCILA_UTILITIES_VERSION_REVISION 0
 
 #include "MycilaExpiringValue.h"
